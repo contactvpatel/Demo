15511e6d0e6645597ecffcbdafabc5130050e30f
PratikProjecttree <pratik.vaghasiya@projecttree.in>
2024-09-06 14:10:58 +0530

    Update CustomerModel.cs

--- src/Demo.Core/Models/CustomerModel.cs
+++ src/Demo.Core/Models/CustomerModel.cs
@@ -6,22 +6,35 @@ namespace Demo.Core.Models
 {
     public class CustomerModel : Entity
     {
+        [FilterMapping("CustomerId")]
         public int CustomerId { get; set; }
+        [FilterMapping("NameStyle")]
         public bool NameStyle { get; set; }
+        [FilterMapping("Title")]
         public string Title { get; set; }
         [FilterMapping("FirstName")]
         public string FirstName { get; set; } = null!;
+        [FilterMapping("MiddleName")]
         public string MiddleName { get; set; }
         [FilterMapping("LastName")]
         public string LastName { get; set; } = null!;
+        [FilterMapping("Suffix")]
         public string Suffix { get; set; }
+        [FilterMapping("CompanyName")]
         public string CompanyName { get; set; }
+        [FilterMapping("SalesPerson")]
         public string SalesPerson { get; set; }
+        [FilterMapping("EmailAddress")]
         public string EmailAddress { get; set; }
+        [FilterMapping("Phone")]
         public string Phone { get; set; }
+        [FilterMapping("PasswordHash")]
         public string PasswordHash { get; set; } = null!;
+        [FilterMapping("PasswordSalt")]
         public string PasswordSalt { get; set; } = null!;
+        [FilterMapping("Rowguid")]
         public Guid Rowguid { get; set; }
+        [FilterMapping("ModifiedDate")]
         public DateTime ModifiedDate { get; set; }
         [JsonIgnore]
         public dynamic CustomerAddresses { get; set; }
